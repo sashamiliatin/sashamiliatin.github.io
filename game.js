3e459f8bb8416ca93528659e0c3a14697ba13a40
sashamiliatin <sashamiliatin@gmail.com>
2017-12-25 07:48:38 +0200

    update1

---
+++ game.js
@@ -0,0 +1,24 @@
+﻿var S = {
+   score:0,ans:"ans"
+ };
+function myFunction() {
+    var x, text;
+    x = document.getElementById("ans").value;
+    if (x == null || x == "") {
+        text = "please enter an answer";
+        document.getElementById("ans").focus();
+    } else if (x == "hamburger" || x=="pizza" || x=="sushi" || x=="salad" || x=="strawberry") {
+         if(S.ans!=x)
+         {
+              S.score=S.score+20;
+         }
+        text = "great job!";
+    }
+    else {
+        document.getElementById("ans").focus();
+        text = "please try again";
+    }
+    document.getElementById("check").innerHTML = text;
+    document.getElementById("qty").value = S.score;
+S.ans=x;
+}
