f7a86a03f475749a5458784ecfe757d984e6e274
sashamiliatin <34767366+sashamiliatin@users.noreply.github.com>
2018-01-15 06:58:51 +0200

    Update myfunctions.js

--- myfunctions.js
+++ myfunctions.js
@@ -207,7 +207,7 @@ function nextRoundPractice() {
             resetTimer();
             break;
         case "first stage: hard":
-            window.open("explane1.html","_self");
+            window.open("inst.html","_self");
             break;
     }
 }
